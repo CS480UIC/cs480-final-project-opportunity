1cf5a057b67a021461b7d7009f233e6b21045193
Christina Wen <cwen7@uic.edu>
2021-11-11 22:14:09 -0600

    Added queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -9,22 +9,29 @@ ORDER BY salary DESC;
 SELECT *
 FROM scholarship AS s
 WHERE s.reward_amount > 3000 AND s.deadline = '2021-11-11'
-ORDER BY s.award DESC;
+ORDER BY s.award_name DESC;
 
+SELECT * 
+FROM tutor 
+WHERE grade_level > 9 AND fee < 30
+ORDER BY grade_level DESC;
 
 /* ######## */
 
 /* Aggregate Queries: An aggregate query groups data in one table and applies an aggregate function */
 
-SELECT user_id, award_name, `description`, MAX(reward_amount), deadline
+SELECT user_id, award_name, description, MAX(reward_amount), deadline
 FROM scholarship
-GROUP BY user_id
+GROUP BY user_id, award_name, description, deadline
 HAVING deadline > '2022-03-01';
 
 SELECT subject, COUNT(*) AS totalTutors
 FROM tutor
 GROUP BY subject;
 
+SELECT legal_id, user_id, cost, legal_description, resource_location, resource_name
+FROM legal_resource
+GROUP BY legal_id, user_id, cost, legal_description, resource_location, resource_name;
 
 /* ######## */
 
@@ -37,3 +44,8 @@ ON job_opportunity.company_id = company.company_id;
 
 SELECT fullname, subject, fee, (SELECT service_name FROM tutoring_service ts WHERE ts.service_id = t.service_id) AS service_name
 FROM tutor t;
+
+SELECT username, cost, legal_description 
+FROM user AS U
+INNER JOIN legal_resource AS LR
+ON U.user_id = LR.user_id;
