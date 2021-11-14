2bddcd8c56c5bf4d1721f421a5b8398e7d6bcf53
Jessica <jborow7@uic.edu>
2021-11-13 23:11:30 -0600

    Updated queries.sql and storep_functions.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,6 +1,4 @@
-/* Simple Queries: A simple query searches data in one table using logic operators in the condition,
-   and orders the result by a particular column. */
-
+# Simple Queries
 SELECT position_title, job_description, salary
 FROM job_opportunity
 WHERE salary > 70000 AND benefit_description LIKE '%Insurance%'
@@ -9,43 +7,54 @@ ORDER BY salary DESC;
 SELECT *
 FROM scholarship AS s
 WHERE s.reward_amount > 3000 AND s.deadline = '2021-11-11'
-ORDER BY s.award_name DESC;
+ORDER BY s.reward_amount DESC;
 
 SELECT * 
 FROM tutor 
 WHERE grade_level > 9 AND fee < 30
 ORDER BY grade_level DESC;
 
-/* ######## */
+SELECT username, date_time
+FROM user
+WHERE username LIKE '%7%'
+ORDER BY date_time;
 
-/* Aggregate Queries: An aggregate query groups data in one table and applies an aggregate function */
-
-SELECT user_id, award_name, description, MAX(reward_amount), deadline
+# Aggregate Queries
+SELECT user_id, award_name, `description`, MAX(reward_amount), deadline
 FROM scholarship
-GROUP BY user_id, award_name, description, deadline
+GROUP BY user_id, award_name, `description`, deadline
 HAVING deadline > '2022-03-01';
 
-SELECT subject, COUNT(*) AS totalTutors
-FROM tutor
+SELECT subject, COUNT(*) AS totalTutors 
+FROM tutor 
 GROUP BY subject;
 
-SELECT legal_id, user_id, cost, legal_description, resource_location, resource_name
+SELECT legal_id, user_id, cost, legal_description, resource_location, resource_name, 
+concat(resource_name, " ", legal_description) as NameDescription
 FROM legal_resource
 GROUP BY legal_id, user_id, cost, legal_description, resource_location, resource_name;
 
-/* ######## */
+SELECT date_time, COUNT(*) AS NumUsers
+FROM user
+GROUP BY MINUTE(date_time);
 
-/* Complex Queries: A complex query searches data in two or more tables */
-
+# Complex Queries
 SELECT position_title, job_description, salary, `name`, industry, review
 FROM job_opportunity
 INNER JOIN company
 ON job_opportunity.company_id = company.company_id;
 
-SELECT fullname, subject, fee, (SELECT service_name FROM tutoring_service ts WHERE ts.service_id = t.service_id) AS service_name
+SELECT fullname, `subject`, fee, (SELECT service_name FROM tutoring_service ts WHERE ts.service_id = t.service_id) AS service_name 
 FROM tutor t;
 
 SELECT username, cost, legal_description 
 FROM user AS U
 INNER JOIN legal_resource AS LR
 ON U.user_id = LR.user_id;
+
+SELECT username
+FROM user
+WHERE EXISTS
+	(SELECT *
+     FROM scholarship
+     WHERE user.user_id = scholarship.user_id);
--- sql/storep_functions.sql
+++ sql/storep_functions.sql
@@ -7,25 +7,18 @@ BEGIN
 END$$
 DELIMITER ;
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `biggest_scholarships`()
-BEGIN
-SELECT scholarship_id, award_name, requirement, reward_amount
-FROM scholarship
-WHERE reward_amount > (SELECT AVG(reward_amount) FROM scholarship);
-END
-
 # Functions
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` FUNCTION `CalculateTutorCost`(id INT, hours DECIMAL(8,2)) RETURNS decimal(8,2)
     READS SQL DATA
 BEGIN
 	DECLARE total_cost DECIMAL(8,2);
-
+    
     SELECT fee * hours
     INTO total_cost
-    FROM tutor
+    FROM tutorGetServiceAndContact
     WHERE tutor_id = id;
-
+    
 RETURN total_cost;
 END$$
 DELIMITER ;
